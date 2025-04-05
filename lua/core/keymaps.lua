1a5b55abfb1ce1c1bc24b2567c6777b0ba2a12ce
Pradene <liam.pradene@gmail.com>
2025-04-05 19:03:41 +0200

    Auto pairing ({

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -3,8 +3,9 @@ vim.g.maplocalleader = " "
 
 local keymap = vim.keymap.set
 
--- Exit insert mode
+-- Exit
 keymap("i", ";;", "<ESC>", { desc = "Exit insertion mode with ;;" })
+keymap("v", ";;", "<ESC>", { desc = "Exit visual mode with ;;" })
 
 -- Move selected lines
 keymap("v", "<A-k>", ":m '>-2<CR>gv=gv", { desc = "Move selected text down in visual mode" })
@@ -12,3 +13,7 @@ keymap("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selected text up in visu
 
 -- File explorer
 keymap("n", "<leader>ep", vim.cmd.Ex, { desc = "Explore files" })
+
+-- Autopair
+keymap("i", "(", "()<Left>", { noremap = true, silent = true})
+keymap("i", "{", "{}<Left>", { noremap = true, silent = true})
