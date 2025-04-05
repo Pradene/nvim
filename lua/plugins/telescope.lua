403e18826d0dc84ff6c681f340617ebd5ec5fe7a
Pradene <liam.pradene@gmail.com>
2025-04-06 00:34:46 +0200

    Moving inside telescope entries with C-p/n

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,7 +3,21 @@ return {
   tag = '0.1.8',
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
-    require("telescope").setup()
+    local actions = require("telescope.actions")
+    require("telescope").setup({
+      defaults = {
+        mappings = {
+          i = { -- Insert mode (when typing in Telescope prompt)
+            ["<C-n>"] = actions.move_selection_next, -- Next item
+            ["<C-p>"] = actions.move_selection_previous, -- Previous item
+          },
+          n = { -- Normal mode (when navigating the list)
+            ["<C-n>"] = actions.move_selection_next,
+            ["<C-p>"] = actions.move_selection_previous,
+          },
+        },
+      },
+    })
 
     local builtin = require("telescope.builtin")
     vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
