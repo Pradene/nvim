5d83309f0ee2747c21b40fba7961adadf06698e7
Pradene <liam.pradene@gmail.com>
2025-04-07 16:52:13 +0200

    Save

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,23 +1,15 @@
 return {
-	"folke/flash.nvim",
-	event = "VeryLazy",
-	opts = {},
-	keys = {
-		{
-			"<leader>z",
-			mode = { "n", "x", "o" },
-			function()
-				require("flash").jump()
-			end,
-			desc = "Flash",
-		},
-		{
-			"<leader>Z",
-			mode = { "n", "x", "o" },
-			function()
-				require("flash").treesitter()
-			end,
-			desc = "Flash Treesitter",
-		},
-	},
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  opts = {},
+  keys = {
+    {
+      "<leader>z",
+      mode = { "n", "x", "o" },
+      function()
+        require("flash").jump()
+      end,
+      desc = "Flash",
+    },
+  }
 }
