65a6d15895522203c3101424589ad23ffdce58f2
Pradene <liam.pradene@gmail.com>
2025-04-09 13:30:27 +0200

    Adding noice for command line sweatness

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,27 +1,27 @@
 -- Exit
-vim.keymap.set({ "i", "v" }, ";;", "<ESC>", { desc = "Exit mode with ;;" })
+vim.keymap.set({ "i", "v" }, ";;", "<ESC>", { silent = true, desc = "Exit mode with ;;" })
 
 -- Move selected lines
-vim.keymap.set("v", "<A-k>", ":m '>-2<CR>gv=gv", { desc = "Move selected text down in visual mode" })
-vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selected text up in visual mode" })
+vim.keymap.set("v", "<A-k>", ":m '>-2<CR>gv=gv", { silent = true, desc = "Move selected text down in visual mode" })
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { silent = true, desc = "Move selected text up in visual mode" })
 
 -- Remove highlight on search
-vim.keymap.set("n", "<leader>/", ":noh<CR>", { desc = "Remove highlight search" })
+vim.keymap.set("n", "<leader>/", ":noh<CR>", { silent = true, desc = "Remove highlight search" })
 
-vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "Delete and paste selection" })
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank inside system clipboard" })
+vim.keymap.set("x", "<leader>p", [["_dP]], { silent = true, desc = "Delete and paste selection" })
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { silent = true, desc = "Yank inside system clipboard" })
 
-vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save buffer" })
-vim.keymap.set("n", "<leader>q", ":q<CR>", { desc = "Close buffer" })
+vim.keymap.set("n", "<leader>w", ":w<CR>", { silent = true, desc = "Save buffer" })
+vim.keymap.set("n", "<leader>q", ":q<CR>", { silent = true, desc = "Close buffer" })
 
-vim.keymap.set("n", "<leader>tn", ":tabnew<CR>", { desc = "Open buffer inside a new tab" })
-vim.keymap.set("n", "<leader>tc", ":tabclose<CR>", { desc = "Close buffer inside a new tab" })
-vim.keymap.set("n", "<leader>tt", ":tabnext<CR>", { desc = "Move buffer inside a new tab" })
-vim.keymap.set("n", "<leader>tp", ":tabprevious<CR>", { desc = "Move buffer inside a new tab" })
-vim.keymap.set("n", "<leader>h", ":split<CR>", { desc = "Split terminal horizontaly" })
-vim.keymap.set("n", "<leader>v", ":vsplit<CR>", { desc = "Split terminal verticaly" })
+vim.keymap.set("n", "<leader>tn", ":tabnew<CR>", { silent = true, desc = "Open buffer inside a new tab" })
+vim.keymap.set("n", "<leader>tc", ":tabclose<CR>", { silent = true, desc = "Close buffer inside a new tab" })
+vim.keymap.set("n", "<leader>tt", ":tabnext<CR>", { silent = true, desc = "Move buffer inside a new tab" })
+vim.keymap.set("n", "<leader>tp", ":tabprevious<CR>", { silent = true, desc = "Move buffer inside a new tab" })
+vim.keymap.set("n", "<leader>h", ":split<CR>", { silent = true, desc = "Split terminal horizontaly" })
+vim.keymap.set("n", "<leader>v", ":vsplit<CR>", { silent = true, desc = "Split terminal verticaly" })
 
-vim.keymap.set("n", "<A-h>", "<C-w>h", { desc = "Move to buffer left" })
-vim.keymap.set("n", "<A-l>", "<C-w>l", { desc = "Move to buffer right" })
-vim.keymap.set("n", "<A-j>", "<C-w>j", { desc = "Move to buffer down" })
-vim.keymap.set("n", "<A-k>", "<C-w>k", { desc = "Move to buffer up" })
+vim.keymap.set("n", "<A-h>", "<C-w>h", { silent = true, desc = "Move to buffer left" })
+vim.keymap.set("n", "<A-l>", "<C-w>l", { silent = true, desc = "Move to buffer right" })
+vim.keymap.set("n", "<A-j>", "<C-w>j", { silent = true, desc = "Move to buffer down" })
+vim.keymap.set("n", "<A-k>", "<C-w>k", { silent = true, desc = "Move to buffer up" })
---
+++ lua/plugins/noice.lua
@@ -0,0 +1,66 @@
+return {
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- add any plugin options here if needed
+		},
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+			"rcarriga/nvim-notify",
+		},
+		config = function()
+			require("noice").setup({
+				lsp = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					-- override the lsp markdown formatter with Noice
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true,
+					-- additional LSP configurations can be set here
+				},
+				views = {
+					cmdline_popup = {
+						position = {
+							row = 5,
+							col = "50%",
+						},
+						size = {
+							width = 60,
+							height = "auto",
+						},
+					},
+					popupmenu = {
+						relative = "editor",
+						position = {
+							row = 8,
+							col = "50%",
+						},
+						size = {
+							width = 60,
+							height = 10,
+						},
+						border = {
+							style = "rounded",
+							padding = { 0, 1 },
+						},
+						win_options = {
+							winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+						},
+					},
+					override = {
+						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+						["vim.lsp.util.stylize_markdown"] = true,
+						["cmp.entry.get_documentation"] = true,
+					},
+				},
+				presets = {
+					bottom_search = false,
+					command_palette = true,
+					long_message_to_split = true,
+					inc_rename = false,
+					lsp_doc_border = true,
+				},
+			})
+		end,
+	},
+}
