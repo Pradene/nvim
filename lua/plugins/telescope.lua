2ea401a9f0d1efbbbb2c1c0dc1ac4a58b2c80689
Pradene <liam.pradene@gmail.com>
2025-04-10 14:57:00 +0200

    Find inside opened buffers

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,7 +8,7 @@ return {
       defaults = {
         mappings = {
           i = {
-            ["<C-n>"] = actions.move_selection_next, -- Next item
+            ["<C-n>"] = actions.move_selection_next,     -- Next item
             ["<C-p>"] = actions.move_selection_previous, -- Previous item
           },
           n = {
@@ -22,6 +22,7 @@ return {
     local builtin = require("telescope.builtin")
     vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
     vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Grep" })
+    vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffer" })
     vim.keymap.set("n", "<leader>ds", builtin.lsp_document_symbols, { desc = "Document symbols" })
   end,
 }
