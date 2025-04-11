319efb96d86972aaa1a2a74c8c446c88e6905243
Pradene <liam.pradene@gmail.com>
2025-04-11 02:36:39 +0200

    Some remap for flash

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,15 +1,23 @@
 return {
-  "folke/flash.nvim",
-  event = "VeryLazy",
-  opts = {},
-  keys = {
-    {
-      "<leader>z",
-      mode = { "n", "x", "o" },
-      function()
-        require("flash").jump()
-      end,
-      desc = "Flash",
-    },
-  }
+	"folke/flash.nvim",
+	event = "VeryLazy",
+	opts = {},
+	keys = {
+		{
+			"z",
+			mode = { "n", "x", "o" },
+			function()
+				require("flash").jump()
+			end,
+			desc = "Flash",
+		},
+		{
+			"r",
+			mode = "o",
+			function()
+				require("flash").remote()
+			end,
+			desc = "Remote Flash",
+		},
+	},
 }
