1a860132710b2e9c70a938411b7048f1927f4b8f
Pradene <liam.pradene@gmail.com>
2025-04-14 12:47:24 +0200

    Replacing some keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,11 +5,19 @@ vim.keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode" })
 vim.keymap.set("v", "<A-k>", ":m '>-2<CR>gv=gv", { silent = true, desc = "Move selected text down in visual mode" })
 vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { silent = true, desc = "Move selected text up in visual mode" })
 
+vim.keymap.set({ "n", "x", "v" }, "gl", "$", { noremap = true })
+vim.keymap.set({ "n", "x", "v" }, "gh", "_", { noremap = true })
+
+vim.keymap.set("v", ">", ">gv", { desc = "Indent doesn't escape visual mode" })
+vim.keymap.set("v", "<", "<gv", { desc = "Indent doesn't escape visual mode" })
+
 -- Remove highlight on search
 vim.keymap.set("n", "<leader>/", ":noh<CR>", { silent = true, desc = "Remove highlight search" })
 
 vim.keymap.set("x", "<leader>p", [["_dP]], { silent = true, desc = "Delete and paste selection" })
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { silent = true, desc = "Yank inside system clipboard" })
+
+vim.keymap.set("n", "U", "<C-r>", { noremap = true, desc = "Redo with U" })
 
 vim.keymap.set("n", "<leader>w", ":w<CR>", { silent = true, desc = "Save buffer" })
 vim.keymap.set("n", "<leader>q", ":q<CR>", { silent = true, desc = "Close buffer" })
@@ -21,7 +29,7 @@ vim.keymap.set("n", "<leader>tp", ":tabprevious<CR>", { silent = true, desc = "M
 vim.keymap.set("n", "<leader>h", ":split<CR>", { silent = true, desc = "Split terminal horizontaly" })
 vim.keymap.set("n", "<leader>v", ":vsplit<CR>", { silent = true, desc = "Split terminal verticaly" })
 
-vim.keymap.set("n", "<A-h>", "<C-w>h", { silent = true, desc = "Move to buffer left" })
-vim.keymap.set("n", "<A-l>", "<C-w>l", { silent = true, desc = "Move to buffer right" })
-vim.keymap.set("n", "<A-j>", "<C-w>j", { silent = true, desc = "Move to buffer down" })
-vim.keymap.set("n", "<A-k>", "<C-w>k", { silent = true, desc = "Move to buffer up" })
+vim.keymap.set("n", "<C-h>", "<C-w>h", { silent = true, desc = "Move to buffer left" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { silent = true, desc = "Move to buffer right" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { silent = true, desc = "Move to buffer down" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { silent = true, desc = "Move to buffer up" })
